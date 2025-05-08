6673cf3880835b5b3f2dba3e13576b5ba420ae8c
NguyenHuynhHuuLoi <thangloikk2@gmail.com>
2025-05-08 22:25:51 +0700

    env.dart budget-feature

---
+++ lib/config/env.dart
@@ -0,0 +1,5 @@
+final String baseUrl = "https://sepjbbfxdkrbnzuiczhc.supabase.co";
+final String apiKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcGpiYmZ4ZGtyYm56dWljemhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNDQ1MTgsImV4cCI6MjA2MDgyMDUxOH0.tA-ZWseTlYY637UehB2U3L4MItSACYZfBzUF_oAPDqs";
+final String authUrl = "$baseUrl/auth/v1";
+
+

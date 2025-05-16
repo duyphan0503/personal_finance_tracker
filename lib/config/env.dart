79281d15fb00ae7e4ad585379637ebed912e4eb3
NguyenHuynhHuuLoi <thangloikk2@gmail.com>
2025-05-16 23:55:12 +0700

    fix feature/report

--- lib/config/env.dart
+++
@@ -1,5 +0,0 @@
-const String supUrl = "https://klakpukphqlawzcntdxe.supabase.co";
-const String supApiKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYWtwdWtwaHFsYXd6Y250ZHhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MjA5NTIsImV4cCI6MjA2MjE5Njk1Mn0.bF5hvAYfm9zEUbhRYpDHiy_16sAMIJzb5xuRm1zCrNY";
-
-const String authUrl = "$supUrl/auth/v1";
